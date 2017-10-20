7900e1becd7573c3d683a3a0b8ee29758543d443
andrius4669 <andrius4669@gmail.com>
2017-10-20 15:06:24 +0300

    make config more robust

--- src/config.cpp
+++ src/config.cpp
@@ -1,13 +1,20 @@
 #include "config.h"
 
 Config::Config() {
-	cfg = cpptoml::parse_file("config.toml");
+	try {
+		cfg = cpptoml::parse_file("config.toml");
+	}
+	catch(...) {}
 }
 
 Config::~Config() {}
 
 std::string Config::getLogFilename() const
 {
-	auto lf = cfg->get_as<std::string>("log.file");
-	return lf ? *lf : "log.txt";
+	auto defval = "log.txt";
+	if (cfg) {
+		auto lf = cfg->get_as<std::string>("log.file");
+		return lf ? *lf : defval;
+	}
+	return defval;
 }
